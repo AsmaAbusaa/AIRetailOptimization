b290b014a8d660105856e38898c837ea64f82bf7
Asma Abu Saa <asmaabusaa4@gmail.com>
2024-01-31 22:52:05 +0200

    add session and file

--- AIOptimization_api.py
+++ AIOptimization_api.py
@@ -1,154 +1,170 @@
-from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
+from flask import Flask,jsonify,session
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 from xgboost import XGBRegressor
+#from sklearn.metrics import mean_squared_error
+from werkzeug.exceptions import BadRequest
 import os
 
 app = Flask(__name__)
-api = Api(app)
-# Placeholder for storing the processed data
-processed_data = None
-daily_orders_df=None
-filtered_items=None
-items_features=[]
-target=None
-item_models = {}
-processed_orders=False
-weekly_item_sales=pd.read_csv('seed_weekly.csv')
-weekly_item_sales.drop('Unnamed: 0',axis=1,inplace=True)
-df = pd.read_csv('file_out2.csv')
-df.drop(["Unnamed: 0","TotalSales","Discount","CustomerID"], axis=1, inplace = True)
-df['Date']=pd.to_datetime(df['Date'])
-df=df.drop( df.query(" `Quantity`==0 ").index)    
-processed_data = df
+app.secret_key='53f57a96faf2d75f32d5c4e9ce2e8754163a17cd7874b356'
 
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'csv'}
-REQUIRED_COLUMNS = {'InvoiceID', 'ProductID', 'Date', 'Quantity'}
+items_features=["Month", "Year", "Week", "Quantity_Lag_1", "Quantity_Lag_2", "Monthly_Sum", "Monthly_Avg", "min", "max"]
+target ='Quantity'
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#@app.route('/processed', methods=['GET'])
+@app.errorhandler(BadRequest)
+def handle_bad_request(error):
+    return jsonify({'error': 'Bad request'}), 400
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.errorhandler(404)
+def handle_not_found(error):
+    return jsonify({'error': 'Not found'}), 404
 
-class UploadCSV(Resource):
-    def post(self):
-        if 'file' not in request.files:
-            return {'message': 'No file part'}, 400
+@app.route('/process/<file_id>', methods=['POST']) # take file id / name
+def process_file(file_id):
+    try:
+       file_path = os.path.join('uploads', f'{file_id}.csv')
+       df = pd.read_csv(file_path)
 
-        file = request.files['file']
+    except Exception as e:
+        return jsonify({'error': f'Error reading CSV file: {str(e)}'}), 400
+    
+    df.drop(["Unnamed: 0","TotalSales","Discount","CustomerID"], axis=1, inplace = True)
+    df['Date']=pd.to_datetime(df['Date'])
+    processed_data=df
 
-        if file.filename == '':
-            return {'message': 'No selected file'}, 400
+    session['file_path']= file_path
 
-        if file and allowed_file(file.filename):
-            # Create the uploads folder if it doesn't exist
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])
-
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filepath)
-            
-            df_ = pd.read_csv(filepath)
-
-            # Check if the DataFrame contains required columns
-            if not set(REQUIRED_COLUMNS).issubset(df.columns):
-                return {'message': f'File must contain columns: {", ".join(REQUIRED_COLUMNS)}'}, 400
-
-            return {'message': 'File uploaded and saved successfully', 'data': df.to_dict()}, 200
-        else:
-            return {'message': 'Invalid file type'}, 400
-
-api.add_resource(UploadCSV, '/upload')
-
-@app.route('/processed', methods=['GET'])
-def preprocessing_file():
-    global processed_data,daily_orders_df,items_features,target,processed_orders
-    if processed_data.notnull:
-        #Daily orders processing data for the next day
-        daily_orders_df=df.set_index('Date')
+    basket = (processed_data 
+            .groupby(['InvoiceID', 'ProductID'])['Quantity'] 
+            .sum().unstack().reset_index().fillna(0) 
+            .set_index('InvoiceID'))
         
-        daily_orders_df = daily_orders_df['Quantity'].resample('D').count() 
+    frq_items = apriori(basket.astype(bool), min_support=.002, use_colnames=True)
+    rules = association_rules(frq_items, metric="lift", min_threshold=1)
+    rules = rules.sort_values(['confidence', 'lift'], ascending =[False, False])
+
+    # Extract unique items from the 'antecedents' and 'consequents' columns
+    all_items_list = []
+    for rule_items in rules['antecedents'].tolist() + rules['consequents'].tolist():
+        for item in rule_items:
+            if item not in all_items_list:
+                all_items_list.append(item)
+
+    session['sorted_products']=all_items_list
+    
+    return jsonify({'message': 'File uploaded and processed successfully'}), 200
+
+@app.route('/products', methods=['GET']) # /Products
+def mine_association_rules():
+    try:
+        product_ids = session.get('sorted_products')
+
+        if product_ids is None:
+            return jsonify({'error': 'No processed data available'}), 404
+        return jsonify({'itemsets':  product_ids})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/orders', methods=['GET'])
+def predict_orders():
+    try:
+        file_path=session.get('file_path')
+        df = pd.read_csv(file_path)
+
+        if file_path is None:
+            raise BadRequest('No processed data available for daily orders')
+        
+        df.drop(["Unnamed: 0","TotalSales","Discount","CustomerID"], axis=1, inplace = True)
+        df['Date']=pd.to_datetime(df['Date'])
+        
+        daily_orders_df=df.set_index('Date')
+        daily_orders_df = daily_orders_df['InvoiceID'].resample('D').count() 
         daily_orders_df=pd.DataFrame({'Date':daily_orders_df.index,'Orders_Count':daily_orders_df.values})
         daily_orders_df.fillna(0,inplace=True)
         daily_orders_df=daily_orders_df.set_index('Date')
         daily_orders_df=create_time_features(daily_orders_df)
         daily_orders_df=add_lags(daily_orders_df)
-        processed_orders=True
-        items_features=["Month", "Year", "Week", "Quantity_Lag_1", "Quantity_Lag_2", "Monthly_Sum", "Monthly_Avg", "min", "max"]
-        target='Quantity'
-        return jsonify({'message': 'File uploaded and processed successfully'})
 
-    return jsonify({'error': 'Invalid file format'})
-
-@app.route('/products', methods=['GET']) # /Products
-def mine_association_rules():
-    global processed_data,filtered_items
-    basket = (processed_data 
-          .groupby(['InvoiceID', 'ProductID'])['Quantity'] 
-          .sum().unstack().reset_index().fillna(0) 
-          .set_index('InvoiceID'))
-    frq_items = apriori(basket.astype(bool), min_support=.001, use_colnames=True)
-    rules = association_rules(frq_items, metric="lift", min_threshold=1)
-    # Step 1: Filter based on Support
-    min_support_threshold = 0.001  # Set your desired support threshold
-    filtered_rules_support = rules[rules['support'] > min_support_threshold]
-
-    # Step 2: Filter based on Confidence
-    min_confidence_threshold = 0.1  # Set your desired confidence threshold
-    filtered_rules_confidence = filtered_rules_support[filtered_rules_support['confidence'] > min_confidence_threshold]
-    # Extract unique items from the 'antecedents' and 'consequents' columns
-    all_items = set(item for rule_items in filtered_rules_confidence['antecedents'].tolist() + filtered_rules_confidence['consequents'].tolist() for item in rule_items)
-    filtered_items= list(all_items)
+        future=create_future_df(daily_orders_df)
+        future_X=future.drop(['Orders_Count','isFuture'], axis=1)
     
-    return jsonify({'itemsets':  filtered_items})
+        model,X_test,y_test=build_orders_model(daily_orders_df)
 
-@app.route('/orders', methods=['GET'])
-def predict_orders():
-    global daily_orders_df
-    
-    if not processed_orders:
-        return jsonify({'error': 'you must process dataset before'})
-    
-    future=create_future_df(daily_orders_df)
-    future_X=future.drop(['Orders_Count','isFuture'], axis=1)
-   
-    model,X_test,y_test=build_orders_model(daily_orders_df)
-    future['Orders_Count']=model.predict(future_X)
-    X_test['Orders_Count']=model.predict(X_test)
-    predicted_df=pd.concat([X_test,future])
+        future['Orders_Count']=model.predict(future_X)
+        X_test['Orders_Count']=model.predict(X_test)
+        predicted_df=pd.concat([X_test,future])
 
-    return plot_actual_vs_predicted(y_test.index,y_test,predicted_df.index,predicted_df['Orders_Count'],0,0)
+        result = {
+            'actual_dates': list(y_test.index),
+            'actual_orders': list(y_test),
+            'predicted_dates': list(predicted_df.index),
+            'predicted_orders': list(predicted_df['Orders_Count'])
+        }
+        #mae = mean_absolute_error(y_test, X_test['Orders_Count'])
+        return jsonify(result)
+    
+    except BadRequest as e:
+        return handle_bad_request(e)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/predict/<int:item_id>', methods=['GET'])
 def predict_for_item(item_id):
-    global items_features,item_models
-    processed_item=pd.DataFrame()
-    max=df['Date'].max()
-    min=df['Date'].min()
+    global items_features,target
+    try:
+        file_path=session.get('file_path')
+        df = pd.read_csv(file_path)
 
-    item=df.loc[df['ProductID']==item_id]
+        if file_path is None:
+            raise BadRequest('No processed data available for daily orders')
+        
+        df.drop(["Unnamed: 0","TotalSales","Discount","CustomerID"], axis=1, inplace = True)
+        df['Date']=pd.to_datetime(df['Date'])
+        processed_data=df
 
-    item=rebuild_weekly_dataset_for_item(item,min,max)
-    item['Date']=item['Date'].dt.to_timestamp()
-    item=item.set_index('Date')
-    processed_item=engineer_weekly_item_sales_features_for_product(item)
-    processed_item=processed_item.set_index('Date')
+        processed_item=pd.DataFrame()
+        max=processed_data['Date'].max()
+        min=processed_data['Date'].min()
 
-    if item_id not in item_models:
+        item=processed_data.loc[processed_data['ProductID']==item_id]
+
+        item=rebuild_weekly_dataset_for_item(item,min,max)
+        item['Date']=item['Date'].dt.to_timestamp()
+        item=item.set_index('Date')
+        processed_item=engineer_weekly_item_sales_features_for_product(item)
+        processed_item=processed_item.set_index('Date')
+
         model = train_model_for_item(item_id,processed_item)
-    else :
-        model = item_models[item_id]
 
-    X_train, X_test, y_train, y_test=split_data_for_item_predcition(processed_item,items_features,target)
+        X_train, X_test, y_train, y_test=split_data_for_item_predcition(processed_item,items_features,target)
+        
+        future = create_future_item(processed_item)
+        future_X=future[items_features]
+
+        future['Quantity']=model.predict(future_X)
+        X_test['Quantity']=model.predict(X_test)
+
+        predicted_df=pd.concat([X_test,future])
+
+        result = {
+            'actual_dates': list(y_test.index),
+            'actual_orders': list(y_test),
+            'predicted_dates': list(predicted_df.index),
+            'predicted_orders': list(predicted_df['Quantity'])
+        }
+        rmse_ = mean_squared_error(y_test, X_test['Quantity'], squared=False)
+        return jsonify(result)
     
-    future = create_future_item(processed_item)
-    future_X=future[items_features]
-    feature_df=pd.concat([X_test,future_X])
-    prediction = model.predict(feature_df)
-    prediction=list(prediction)
-    return plot_actual_vs_predicted(y_test.index,y_test,feature_df.index,prediction,item_id,1)
+    except BadRequest as e:
+        return handle_bad_request(e)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+#    return plot_actual_vs_predicted(y_test.index,y_test,predicted_df.index,predicted_df['Quantity'],item_id,1)
 
 def build_orders_model(data):
     X = data.drop('Orders_Count', axis=1)# features 
@@ -209,7 +225,7 @@ def rebuild_weekly_dataset_for_item(item_df,min_date,max_date):
     return items_sales
 
 def engineer_weekly_item_sales_features_for_product(product_data):
-    global weekly_item_sales
+    #global weekly_item_sales
     date=product_data.index
     product_data['Week'] = product_data.index.isocalendar ().week
     product_data['Month'] = product_data.index.month
@@ -231,7 +247,7 @@ def engineer_weekly_item_sales_features_for_product(product_data):
     product_data['min'] = product_data['min'].tolist()
     product_data['max'] = product_data['max'].tolist()
     product_data['Date']=date
-    weekly_item_sales=pd.concat([weekly_item_sales,product_data],axis=0)
+    #weekly_item_sales=pd.concat([weekly_item_sales,product_data],axis=0)
     
     return product_data
 
@@ -265,12 +281,14 @@ def train_model_for_item(item_id,product_data):
     X_train, X_test, y_train, y_test=split_data_for_item_predcition(product_data,items_features,target)
     model.fit(X_train, y_train)
 
-    item_models[item_id] = model
-
     return model
 
 def create_future_item(df):
-    Date = pd.date_range('2023-03-25','2023-04-3', freq='W-SUN')
+    min=df.index.max()
+    min_date= min+pd.Timedelta(days=1)
+    max_date= min_date+pd.Timedelta(days=14)
+    Date = pd.date_range(min_date,max_date, freq='W')
+
     future = pd.DataFrame(index=Date)
     future['isFuture'] = True
 
@@ -290,7 +308,12 @@ def create_future_item(df):
 
 def create_future_df(df):
     df_copy=df.copy()
-    Date = pd.to_datetime(['2023-03-26 00:00:00','2023-03-27 00:00:00'])
+    
+    min=df_copy.index.max()
+    min_date= min+pd.Timedelta(days=1)
+    max_date= min_date+pd.Timedelta(days=1)
+    Date = pd.date_range(min_date,max_date, freq='D')
+
     future = pd.DataFrame(index=Date)
     future['isFuture'] = True
     df_copy['isFuture'] = False
